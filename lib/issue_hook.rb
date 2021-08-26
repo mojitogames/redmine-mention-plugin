42039c9cd6c976e4315762aa18ca05f59bc73a42
jayoung <jayoung@mojitogames.com>
2021-08-26 09:05:36 +0000

    add watcher before create issue

--- lib/issue_hook.rb
+++ lib/issue_hook.rb
@@ -4,12 +4,12 @@ require_dependency 'watcher'
 module Mention
   module IssueHook
     def self.included(base)
-      base.send(:after_create) do |issue|
+      base.send(:before_create) do |issue|
         mentioned_users = issue.description.scan(/@[\w.]+/)
         mentioned_users.each do |mentioned_user|
           username = mentioned_user[1..-1] # Remove the heading ':'
           if user = User.find_by_login(username)
-            Watcher.create(:watchable => issue, :user => user)
+            issue.set_watcher(user)
           end
         end
       end
